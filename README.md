1ba8ce58cdef4f1ab6343af9820eb7791c2780f8
gustavo <gtvtempone@gmail.com>
2021-03-08 16:49:27 -0300

    Criação dos arquivos

---
+++ README.md
@@ -0,0 +1,1 @@
+# blinking-neon-js
---
+++ script.js
@@ -0,0 +1,27 @@
+// Target é o elemento h1
+const target = window.document.getElementsByTagName('h1')[0];
+console.log(target);
+
+//Pega a letra e adiciona o style nela, esse style tem uma ////animação com o tempo aleatório de 0 a 4
+const flickerLetter = letter => `<span style="animation: blink linear ${Math.random()*4}s">${letter}</span>`;
+console.log(flickerLetter);
+
+//Pega a letra do h1 e adiciona uma cor aleatória em hsla 
+const colorLetter = letter => `<span style="color: hsla(${Math.random()*360}, 100%, 80%, 1);">${letter}</span>`;
+console.log(colorLetter);
+
+
+//Transforma em um array, pega cada letra do array e anima com o blink, depois colore cada letra com uma cor aleatória e o resultado é a soma de flickerLetter e colorLeter
+const flickerAndColorText = text =>
+  text
+  .split('')
+  .map(flickerLetter)
+  .map(colorLetter)
+  .join('');
+
+  const neonTemps = target => target.innerHTML = flickerAndColorText(target.textContent);
+
+  neonTemps(target);
+  target.onClick = ({target}) => neonTemps(target);
+
+
